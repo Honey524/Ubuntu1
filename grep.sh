5e41af27f9893b38b1b31b79ccc873ff68f0b205
Honey524 <honey02052004@gmail.com>
2024-12-30 16:58:53 +0530

    grep

--- grep.sh
+++ grep.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 # START #
-echo "hello world\infoo bar" | grep "hello"
+echo  "count the number of lines" 
+grep -c "sales" emp1.lst
+echo ""
+echo "File name matching the pattern"
+grep -l "personnel" emp1.lst
+echo ""
+echo "Checking whole word in a file"
+grep -w "aggarwal" emp1.lst
+echo ""
+echo "Displaying on matched patterns"
+grep -o "g.m." emp1.lst
+echo ""
+echo "Show line number"
+grep -n "sales" emp1.lst
+echo ""
+echo "inverting the pattern"
+grep -v "marketing" emp1.lst
+echo ""
 # END #
